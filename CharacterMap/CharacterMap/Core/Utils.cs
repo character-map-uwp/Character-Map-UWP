e7fdaa75d8488fde8b388671afffcfde72f21bf5
Edi Wang <Edi.Wang@outlook.com>
2016-05-06 16:55:45 +0800

    fix #4

---
+++ CharacterMap/CharacterMap/Core/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace CharacterMap.Core
+{
+    public class Utils
+    {
+        public static void UseDarkTheme()
+        {
+            Edi.UWP.Helpers.UI.ApplyColorToTitleBar(Color.FromArgb(255, 43, 43, 43), Colors.White, Colors.DimGray, Colors.White);
+            Edi.UWP.Helpers.UI.ApplyColorToTitleButton(Color.FromArgb(255, 43, 43, 43), Colors.White, Colors.DimGray, Colors.White, Colors.DimGray, Colors.White, Colors.DimGray, Colors.White);
+            Edi.UWP.Helpers.Mobile.SetWindowsMobileStatusBarColor(Color.FromArgb(255, 43, 43, 43), Colors.DarkGray);
+        }
+
+        public static void UseLightTheme()
+        {
+            var color = Edi.UWP.Helpers.UI.GetAccentColor();
+
+            Edi.UWP.Helpers.Mobile.SetWindowsMobileStatusBarColor(color, Colors.White);
+            Edi.UWP.Helpers.UI.ApplyColorToTitleBar(
+            color,
+            Colors.White,
+            Colors.LightGray,
+            Colors.Gray);
+
+            Edi.UWP.Helpers.UI.ApplyColorToTitleButton(
+                color, Colors.White,
+                color, Colors.White,
+                color, Colors.White,
+                Colors.LightGray, Colors.Gray);
+        }
+    }
+}
---
+++ CharacterMap/CharacterMap/Helpers/AppSettings.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace CharacterMap.Core
+{
+    public class AppSettings : INotifyPropertyChanged
+    {
+        public bool UseDarkThemeSetting
+        {
+            get
+            {
+                return ReadSettings(nameof(UseDarkThemeSetting), false);
+            }
+            set
+            {
+                SaveSettings(nameof(UseDarkThemeSetting), value);
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ApplicationDataContainer LocalSettings { get; set; }
+
+        public AppSettings()
+        {
+            LocalSettings = ApplicationData.Current.LocalSettings;
+        }
+
+        private void SaveSettings(string key, object value)
+        {
+            LocalSettings.Values[key] = value;
+        }
+
+        private T ReadSettings<T>(string key, T defaultValue)
+        {
+            if (LocalSettings.Values.ContainsKey(key))
+            {
+                return (T)LocalSettings.Values[key];
+            }
+            if (null != defaultValue)
+            {
+                return defaultValue;
+            }
+            return default(T);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName]string propName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
