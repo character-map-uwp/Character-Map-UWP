6ac89aa8dd302deb7a4abc8e0bf80f4430c826bc
Edi Wang <Edi.Wang@outlook.com>
2017-08-10 16:51:50 +0800

    Fix title bar

--- CharacterMap/CharacterMap/MainPage.xaml.cs
+++ CharacterMap/CharacterMap/MainPage.xaml.cs
@@ -1,25 +1,73 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
+using Windows.UI.Core;
 using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using CharacterMap.Annotations;
 using CharacterMap.Core;
 using CharacterMap.ViewModel;
 
 namespace CharacterMap
 {
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
         public MainViewModel MainViewModel { get; set; }
 
         public AppSettings AppSettings { get; set; }
+
+        #region Title Bar
+
+        private CoreApplicationViewTitleBar _coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+
+        public Thickness CoreTitleBarPadding
+        {
+            get
+            {
+                if (ApplicationView.GetForCurrentView().IsFullScreenMode)
+                {
+                    return new Thickness(0, 0, 0, 0);
+                }
+                else
+                {
+                    //return new Thickness(0, 0, 140, 0);
+                    return FlowDirection == FlowDirection.LeftToRight ?
+                        new Thickness() { Left = _coreTitleBar.SystemOverlayLeftInset, Right = _coreTitleBar.SystemOverlayRightInset } :
+                        new Thickness() { Left = _coreTitleBar.SystemOverlayRightInset, Right = _coreTitleBar.SystemOverlayLeftInset };
+                }
+            }
+        }
+
+        public double CoreTitleBarHeight => _coreTitleBar.Height;
+
+
+        private void OnWindowSizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            UpdateLayoutMetrics();
+        }
+
+        void OnLayoutMetricsChanged(CoreApplicationViewTitleBar sender, object e)
+        {
+            UpdateLayoutMetrics();
+        }
+
+        void UpdateLayoutMetrics()
+        {
+            OnPropertyChanged(nameof(CoreTitleBarHeight));
+            OnPropertyChanged(nameof(CoreTitleBarPadding));
+        }
+
+        #endregion
 
         public MainPage()
         {
@@ -29,9 +77,8 @@ namespace CharacterMap
 
             AppSettings = new AppSettings();
 
-            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
-            coreTitleBar.ExtendViewIntoTitleBar = true;
-            Window.Current.SetTitleBar(TitleBar);
+            _coreTitleBar.ExtendViewIntoTitleBar = true;
+            Window.Current.SetTitleBar(TitleBarBackgroundElement);
 
             BorderFadeInStoryboard.Completed += async (o, _) =>
             {
@@ -42,6 +89,10 @@ namespace CharacterMap
 
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
+            _coreTitleBar.LayoutMetricsChanged += OnLayoutMetricsChanged;
+            Window.Current.SizeChanged += OnWindowSizeChanged;
+            UpdateLayoutMetrics();
+
             if (null != LstFontFamily.Items)
             {
                 if (AppSettings.UseDefaultSelection)
@@ -142,6 +193,14 @@ namespace CharacterMap
         private void BtnSetDefault_OnClick(object sender, RoutedEventArgs e)
         {
             AppSettings.DefaultSelectedFontName = LstFontFamily.SelectedValue as string;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
