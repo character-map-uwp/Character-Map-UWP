aedec4140576495f5be78491a19789bf33c09a69
Ray Letter <ray.letter@hotmail.com>
2020-03-08 16:03:15 +0700

    Added model for language setting

---
+++ CharacterMap/CharacterMap/Models/SupportedLanguage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterMap.Models
+{
+    public class SupportedLanguage
+    {
+        public string LanguageID;
+        public string LanguageName;
+
+        public SupportedLanguage()
+        {
+            LanguageName = "";
+            LanguageID = "";
+        }
+
+        public SupportedLanguage(string ID)
+        {
+            LanguageID = new System.Globalization.CultureInfo(ID).Name;
+            LanguageName = new System.Globalization.CultureInfo(ID).DisplayName;
+        }
+
+        public override string ToString() => LanguageName;
+
+        public static SupportedLanguage DefaultLanguage => new SupportedLanguage("en-US");
+    }
+}
