a0849b4ddef7bc85b8ffc28b443548f3ea19b4ab
Johnny Westlake <jmdwestlake@yahoo.co.uk>
2020-03-11 12:39:09 +0000

    Fix SVG & XAML paths for languages that use a decimal point specifier other than '.'

--- CharacterMap/CharacterMap/Core/SVGPathReciever.cs
+++ CharacterMap/CharacterMap/Core/SVGPathReciever.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Graphics.Canvas.Geometry;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -19,29 +20,29 @@ namespace CharacterMap.Core
 
         public void BeginFigure(Vector2 startPoint, CanvasFigureFill figureFill)
         {
-            _builder.AppendFormat("M {0} {1} ", startPoint.X, startPoint.Y);
+            _builder.AppendFormat(CultureInfo.InvariantCulture, "M {0} {1} ", startPoint.X, startPoint.Y);
         }
 
         public void AddArc(Vector2 endPoint, float radiusX, float radiusY, float rotationAngle, CanvasSweepDirection sweepDirection, CanvasArcSize arcSize)
         {
-            _builder.AppendFormat("A {0} {1} {2} {3} {4} {5} {6} ", 
+            _builder.AppendFormat(CultureInfo.InvariantCulture, "A {0} {1} {2} {3} {4} {5} {6} ", 
                 radiusX, radiusY, rotationAngle, (int)arcSize, (int)sweepDirection, endPoint.X, endPoint.Y);
         }
 
         public void AddCubicBezier(Vector2 controlPoint1, Vector2 controlPoint2, Vector2 endPoint)
         {
-            _builder.AppendFormat("C {0} {1} {2} {3} {4} {5} ", 
+            _builder.AppendFormat(CultureInfo.InvariantCulture, "C {0} {1} {2} {3} {4} {5} ", 
                 controlPoint1.X, controlPoint1.Y, controlPoint2.X, controlPoint2.Y, endPoint.X, endPoint.Y);
         }
 
         public void AddLine(Vector2 endPoint)
         {
-            _builder.AppendFormat("L {0} {1} ", endPoint.X, endPoint.Y);
+            _builder.AppendFormat(CultureInfo.InvariantCulture, "L {0} {1} ", endPoint.X, endPoint.Y);
         }
 
         public void AddQuadraticBezier(Vector2 controlPoint, Vector2 endPoint)
         {
-            _builder.AppendFormat("Q {0} {1} {2} {3} ", controlPoint.X, controlPoint.Y, endPoint.X, endPoint.Y);
+            _builder.AppendFormat(CultureInfo.InvariantCulture, "Q {0} {1} {2} {3} ", controlPoint.X, controlPoint.Y, endPoint.X, endPoint.Y);
         }
 
         public void SetFilledRegionDetermination(CanvasFilledRegionDetermination filledRegionDetermination)
@@ -49,7 +50,7 @@ namespace CharacterMap.Core
             if (_builder.Length == 0)
                 return;
 
-            _builder.AppendFormat("F {0} ", (int)filledRegionDetermination);
+            _builder.AppendFormat(CultureInfo.InvariantCulture, "F {0} ", (int)filledRegionDetermination);
         }
 
         public void SetSegmentOptions(CanvasFigureSegmentOptions figureSegmentOptions)
