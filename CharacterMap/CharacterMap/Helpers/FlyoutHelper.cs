5627962239b57b1ca7ba81ab1288733a4afa3b84
Johnny Westlake <jmdwestlake@yahoo.co.uk>
2021-10-18 17:17:36 +0100

    Improve hittesting of font list context menus on all themes

--- CharacterMap/CharacterMap/Helpers/FlyoutHelper.cs
+++ CharacterMap/CharacterMap/Helpers/FlyoutHelper.cs
@@ -226,9 +226,11 @@ namespace CharacterMap.Helpers
                     var newCollection = new MenuFlyoutItem
                     {
                         Text = Localization.Get("NewCollectionItem/Text"),
-                        Icon = new FontIcon { Glyph = "\uE109" }
-                };
+                        Icon = new FontIcon { Glyph = "\uE109" },
+                        DataContext = font
+                    };
                     newCollection.Click += CreateCollection_Click;
+
                     if (newColl.Items != null)
                     {
                         newColl.Items.Add(newCollection);
@@ -241,7 +243,8 @@ namespace CharacterMap.Helpers
                             var symb = new MenuFlyoutItem
                             {
                                 Text = Localization.Get("OptionSymbolFonts/Text"),
-                                IsEnabled = !_collections.SymbolCollection.Fonts.Contains(font.Name)
+                                IsEnabled = !_collections.SymbolCollection.Fonts.Contains(font.Name),
+                                DataContext = font
                             };
                             symb.Click += AddToSymbolFonts_Click;
                             newColl.Items.Add(symb);
@@ -321,7 +324,7 @@ namespace CharacterMap.Helpers
                         {
                             Text = Localization.Get("BtnPrint/Content"),
                             Icon = new FontIcon { Glyph = "\uE749" }
-                    }.AddKeyboardAccelerator(VirtualKey.P, VirtualKeyModifiers.Control);
+                        }.AddKeyboardAccelerator(VirtualKey.P, VirtualKeyModifiers.Control);
 
                         item.Click += Print_Click;
                         menu.Items.Insert(standalone ? 2 : 3, item);
@@ -355,7 +358,7 @@ namespace CharacterMap.Helpers
                 {
                     Text = Localization.Get("CompareFontsButton/Text"),
                     Icon = new FontIcon { Glyph = "\uE1D3" }
-            }.AddKeyboardAccelerator(VirtualKey.K, VirtualKeyModifiers.Control);
+                }.AddKeyboardAccelerator(VirtualKey.K, VirtualKeyModifiers.Control);
 
                 qq.Click += (s, e) =>
                 {
@@ -372,7 +375,7 @@ namespace CharacterMap.Helpers
                     {
                         Text = Localization.Get("AddToQuickCompare/Text"),
                         Icon = new FontIcon { Glyph = "\uE109" }
-                }.AddKeyboardAccelerator(VirtualKey.Q, VirtualKeyModifiers.Control);
+                    }.AddKeyboardAccelerator(VirtualKey.Q, VirtualKeyModifiers.Control);
 
                     item.Click += (s, e) =>
                     {
