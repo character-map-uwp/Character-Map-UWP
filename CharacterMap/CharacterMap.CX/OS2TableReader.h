32bf068503b820c4b9d919a6d7811a8ddc0d2dbe
Johnny <jmdwestlake@yahoo.co.uk>
2025-06-02 13:09:36 +0100

    More stringent OS/2 fsType parsing

--- CharacterMap/CharacterMap.CX/OS2TableReader.h
+++ CharacterMap/CharacterMap.CX/OS2TableReader.h
@@ -24,7 +24,7 @@ namespace CharacterMapCX
 		Restricted = 2,
 		PreviewPrint = 4,
 		Editable = 8,
-		NoSubsetting = 265,
+		NoSubsetting = 256,
 		BitmapOnly = 512
 	};
 
@@ -53,27 +53,42 @@ namespace CharacterMapCX
 			// Contains font embedding information
 			FSType = GetUInt16(); 
 
-			//bool installable = (FSType & 0x0002) == 0 && (FSType & 0x0004) == 0 && (FSType & 0x0008) == 0;
-
+			// Parse fsType bits
 			FontEmbeddingType type = FontEmbeddingType::Installable;
-			if (FSType & 0x0002)
-				type = FontEmbeddingType::Restricted;
+			
+			// Bits 0 to 3 must be mutually exclusive.
+			// The specification for versions 0 to 2 did not specify that bits 0 to 3 must be mutually exclusive.
+			// Rather, those specifications stated that, in the event that more than one of bits 0 to 3 are set 
+			// in a given font, then the least - restrictive permission indicated take precedence.
+			// As such, this interpretation of bits 0 to 3 should cover all versions of the OS/2 table.
+			if (FSType & 0x0008)
+				type = FontEmbeddingType::Editable;
 			else if (FSType & 0x0004)
 				type = FontEmbeddingType::PreviewPrint;
-			else if (FSType & 0x0008)
-				type = FontEmbeddingType::Editable;
+			else if (FSType & 0x0002)
+				type = FontEmbeddingType::Restricted;
+		
+			// Some fonts using a version 0 to version 2 OS/2 table have both bit 2 and bit 3 set with the 
+			// intent to indicate both preview/print and edit permissions. Applications are permitted to use 
+			// this behavior for fonts with a version 0 to version 2 OS/2 table.
+			if (Version <= 2 && (FSType & 0x0004) && (FSType & 0x0008))
+				type = FontEmbeddingType::PreviewPrint | FontEmbeddingType::Editable;
 
-			if (FSType & 0x0100)
-				type = type | FontEmbeddingType::NoSubsetting;
+			// Versions 0 to 1: only bits 0 to 3 were assigned. 
+			// Applications must ignore bits 4 to 15 when reading a version 0 or version 1 table.
+			if (Version > 1)
+			{
+				if (FSType & 0x0100)
+					type = type | FontEmbeddingType::NoSubsetting;
 
-			if (FSType & 0x0200)
-				type = type | FontEmbeddingType::BitmapOnly;
+				if (FSType & 0x0200)
+					type = type | FontEmbeddingType::BitmapOnly;
+			}
 
 			EmbeddingType = type;
 
 
 			// Don't care about the rest of the table right now 8)
-			
 		};
 
 	private:
