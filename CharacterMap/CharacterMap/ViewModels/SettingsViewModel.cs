e10d556f77905619edf74d5717368c2085ef2c56
Johnny <jmdwestlake@yahoo.co.uk>
2025-06-11 09:32:55 +0100

    localisation tweaks

--- CharacterMap/CharacterMap/ViewModels/SettingsViewModel.cs
+++ CharacterMap/CharacterMap/ViewModels/SettingsViewModel.cs
@@ -133,7 +133,7 @@ public partial class SettingsViewModel : ViewModelBase
                 "- Added ability to use Ctrl + ScrollWheel to change font size in Font List Pane, Character Map and Quick Compare\n" +
                 "- Hidden variable font axis are now visible and editable in Type Ramp View\n" +
                 "- Added option in Settings->Layout->Font List Display to increase font size of the main font list\n" +
-                "- Added Font Embedding Rights and Supported Script information to Font Face Information flyout"),
+                "- Added Font Embedding Rights and Supported Script information to Font Face Properties flyout"),
              new("2024.8.0.0 (November 2024)", // November 2024
                 "- Simulated font faces are now grouped separately from real fonts faces\n" +
                 "- Added option in Settings->Advanced to hide showing simulated font faces"),
